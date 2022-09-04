21945bab02ad7302de6720cab6687d9777beb6af
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-09-04 16:22:23 -0700

    more dependencies and ports

--- app.js
+++ app.js
@@ -1,6 +1,44 @@
 const butt = "touch";
 const exp = require('express');
+const hbars = require('express-handlebars');
 const expsess = require('express-session');
 const sql2 = require('mysql2');
 const env = require('dotenv');
-const seq = require('sequelize');
+const sequelize = require('sequelize');
+const path = require('path');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const routes = require('./controllers')
+const hbs = hbars.create({ helpers });
+
+
+const sessy = {
+    secret: 'horse',
+    cookie: {
+        expires: 10 * 60 * 1500 //expire in 15 minutes
+    },
+    resave: true,
+    rolling: true,
+    saveUnitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    }),
+};
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//middleware
+app.use(session(sessy));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Working'));
+});
