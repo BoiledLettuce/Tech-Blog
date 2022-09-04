669a733e731f15116d16e89d47fb21d2499eb493
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-09-04 14:55:11 -0700

    dependency thing

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 const butt = "touch";
+const exp = require('express');
+const expsess = require('express-session');
+const sql2 = require('mysql2');
+const env = require('dotenv');
+const seq = require('sequelize');
