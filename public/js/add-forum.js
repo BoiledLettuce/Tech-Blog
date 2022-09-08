2ff73e93f428bc0ca1bd43ea1c88f39cf3032526
Joshua Henson <tktjoshuahenson@yahoo.com>
2022-09-07 19:07:52 -0700

    code for javascript public files

---
+++ public/js/add-forum.js
@@ -0,0 +1,27 @@
+async function newForumHandler(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="forum-title]').value;
+    const forum_content = document.querySelector('input[name="forum-content"]').value;
+
+    const response = await fetch(`/api/forums`, {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            forum_content
+        }),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        console.log(forum_content);
+
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newForumHandler);
